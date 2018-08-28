97d2a8ce235ee1187a7ad1133e6246f279d23e87
Guillaume Sibon <guillaume.infonews@gmail.com>
2018-08-28 13:15:54 +0200

    change text on landing and fix bug github link

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -21,10 +21,18 @@ class Landing extends Component {
                 <h1 className="display-3 mb-4">DevJungle</h1>
                 <p className="lead">
                   {" "}
+                  Are you looking for a web developer? Just click here...
+                </p>
+                <Link to="/profiles" className="btn btn-lg btn-info">
+                  Find my futur web developer
+                </Link>
+                <hr />
+
+                <p className="lead">
+                  {" "}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
-                <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
                 </Link>
